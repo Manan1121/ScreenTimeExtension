395e1077fa307f57cb761d0b8c2d339b9b2b7ece
Manan1121 <mananhshah04@gmail.com>
2023-10-26 00:34:43 -0400

    Initial

--- popup.js
+++ popup.js
@@ -1,128 +1,51 @@
 document.addEventListener('DOMContentLoaded', function() {
-    let trackedTabs = JSON.parse(localStorage.getItem("trackedTabs")) || [];
-    let tabTimers = JSON.parse(localStorage.getItem("tabTimers")) || {};
-    let activeTabId = null;
-    let currentTab = {};
-
+    let tabTimers = {};
+    let darkMode = false;
     const darkModeSwitch = document.getElementById("darkModeSwitch");
     const container = document.getElementsByClassName("container").item(0);
-    const tabBtn = document.getElementById("tab-btn");
-    let ulEL = document.getElementById("ul-el");
-    const clearBtn = document.getElementById("clear-btn")
-    let darkMode = false;
 
-    for (const tab of trackedTabs) {
-        if (!tabTimers[tab.tabId]) {
-            tabTimers[tab.tabId] = {
-                startTime: Date.now(),
-                timeSpent: tab.timeSpent || 0
-            };
-        }
-    }
+    // document.getElementById('tab-btn').addEventListener('click', function () {
+    //     chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+    //         if (tabs.length === 0) return; // No active tab found
+    
+    //         const tab = tabs[0];
+    //         const tabId = tab.id.toString();
+            
+    //         // Check if the tab is already being tracked
+    //         if (!tabTimers[tabId]) {
+    //             tabTimers[tabId] = { timeSpent: 0, url: tab.url };
+                
+    //             // Save the new tab to local storage and update the UI
+    //             chrome.storage.local.set({ tabTimers: tabTimers }, function () {
+    //                 loadTabTimers(); // Reload the rendered list with the new tab
+    //             });
+    //         }
+    //     });
+    // });
 
-    render(trackedTabs);
-
-    //DARK MODE BUTTON START
-    function toggleDarkMode() {
+     //DARK MODE BUTTON START
+     function toggleDarkMode() {
         darkMode = !darkMode;
         container.classList.toggle('dark-mode', darkMode);
     }
 
     darkModeSwitch.addEventListener("change", toggleDarkMode);
-
-    clearBtn.addEventListener("click", function(){
-        localStorage.clear();
-        console.log("clearing")
-        render(trackedTabs);
-    })
-    //DARK MODE BUTTON END
-
-    //Creates a new tab and adds it to the tracked tabs when clicked
-    tabBtn.addEventListener("click", function() {
-        chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
-            const tab = tabs[0];
-            const tabDomain = new URL(tab.url).hostname;
-            
-            //supposed to check if tab already exists 
-            const existingTab = trackedTabs.find(item => new URL(item.url).hostname === tabDomain);
-    
-           // if (!existingTab) {
-                const tabId = tab.id;
-    
-                if (!tabTimers[tabId]) {
-                    tabTimers[tabId] = {
-                        startTime: Date.now(),
-                        timeSpent: 0
-                    };
-                }
-    
-                trackedTabs.push({ tabId, url: tab.url });
-                localStorage.setItem("tabTimers", JSON.stringify(tabTimers));
-                localStorage.setItem("trackedTabs", JSON.stringify(trackedTabs));
-                render(trackedTabs);
-           // }
-        });
-    });
-    
-
-    chrome.tabs.onActivated.addListener(function(activeInfo) {
-        const prevTabId = activeTabId;
-        activeTabId = activeInfo.tabId;
-    
-        if (prevTabId && tabTimers[prevTabId]) {
-            const endTime = Date.now();
-            tabTimers[prevTabId].timeSpent += endTime - tabTimers[prevTabId].startTime;
-            tabTimers[prevTabId].startTime = endTime;
-            updateRenderedTime(prevTabId, tabTimers[prevTabId].timeSpent);
-            localStorage.setItem("tabTimers", JSON.stringify(tabTimers));
-            localStorage.setItem("trackedTabs", JSON.stringify(trackedTabs));
-        }
-    
-        // Start the timer for the newly activated tab
-        if (tabTimers[activeTabId]) {
-            tabTimers[activeTabId].startTime = Date.now();
-        }
-    });
-    
-    // Periodically update the displayed time for tracked tabs
-    setInterval(function() {
-        //console.log(activeTabId)
-        chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
-            currentTab = tabs[0];
-            //console.log(currentTab.id)
-        })
-        for (const tabId in tabTimers) {
-            if (tabTimers.hasOwnProperty(tabId) && tabId == currentTab.id) {
-                const elapsedTime = Date.now() - tabTimers[tabId].startTime;
-                updateRenderedTime(tabId, tabTimers[tabId].timeSpent + elapsedTime);
-                localStorage.setItem("tabTimers", JSON.stringify(tabTimers));
-            }
-        }
-    }, 1000); // Update every second
-
-    
-    
-
-    function updateRenderedTime(tabId, timeSpent) {
-        const tabElement = document.getElementById(`tab-${tabId}`);
-        if (tabElement) {
-            tabElement.querySelector(".time-spent").textContent = formatTime(timeSpent);
-        }
-    }
-
+    // Format time from milliseconds to a readable "X min Y sec" format
     function formatTime(milliseconds) {
-        const seconds = Math.floor(milliseconds / 1000);
-        const minutes = Math.floor(seconds / 60);
-        return `${minutes}m ${seconds % 60}s`;
+        let seconds = Math.floor(milliseconds / 1000);
+        let minutes = Math.floor(seconds / 60);
+        seconds = seconds % 60;
+        return `${minutes} min ${seconds} sec`;
     }
-
+    
+    // Render the list of tabs and times in the popup
     function render(tabs) {
         let listItems = "";
         for (let i = 0; i < tabs.length; i++) {
-            const tabId = tabs[i].tabId;
+            const tabId = tabs[i].id.toString();
             const url = tabs[i].url;
             const timeSpent = tabTimers[tabId] ? tabTimers[tabId].timeSpent : 0;
-
+    
             listItems += `
                 <li id="tab-${tabId}">
                     <a target='_blank' href='${url}'>
@@ -132,6 +55,50 @@ document.addEventListener('DOMContentLoaded', function() {
                 </li>
             `;
         }
-        ulEL.innerHTML = listItems;
+        document.getElementById('ul-el').innerHTML = listItems;
     }
-});
+    
+    // Load tab timers from local storage
+    function loadTabTimers() {
+        chrome.storage.local.get(['tabTimers'], function (result) {
+            if (result.tabTimers) {
+                tabTimers = result.tabTimers;
+            }
+            // Query all tabs to render them with the loaded time data
+            chrome.tabs.query({}, function (tabs) {
+                render(tabs);
+            });
+        });
+    }
+    
+    // Update the time spent for the currently active tab
+    function updateTime() {
+        chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+            if (tabs[0]) {
+                const tabId = tabs[0].id.toString();
+                if (!tabTimers[tabId]) {
+                    tabTimers[tabId] = { timeSpent: 0, url: tabs[0].url };
+                }
+                tabTimers[tabId].timeSpent += 1000; // Increment the timer by 1 second
+    
+                // Save the updated times to local storage
+                chrome.storage.local.set({ tabTimers: tabTimers }, function () {
+                    loadTabTimers(); // Reload the rendered list with updated times
+                });
+            }
+        });
+    }
+    
+    // Clear the time data and update the UI
+    document.getElementById('clear-btn').addEventListener('click', function () {
+        chrome.storage.local.remove(['tabTimers'], function () {
+            tabTimers = {};
+            loadTabTimers();
+        });
+    });
+    
+    // Initialize by loading any saved tab timer data
+    loadTabTimers();
+    // Update time every second
+    setInterval(updateTime, 1000);
+})
